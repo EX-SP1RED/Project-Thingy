925dd2b3de8dffc7371a559203465dabd12a4c6f
EX-SP1RED <ldcook0419@gmail.com>
2026-03-02 21:16:02 +0000

    Second Version

--- main.py
+++ main.py
@@ -8,3 +8,21 @@ elif Order_Number_1 == "Both" :
     print("That will be 80 dollars... We wish you luck...")
 else :
     print("We are Timmy's Tacos... we obviously dont serve tacos or anything else here... lets try again...")
+# The other menu
+Order_Number_2 = input("We want more money! Order more! Would you like a main corse or a desert?")
+if Order_Number_2 == "Main Course" :
+    Main_Course = input("Great! What would you like? Pancakes or Waffles?")
+    if Main_Course == "Pancakes" :
+        print("That will be 25 dollars...")
+    elif Main_Course == "Pancakes" :
+        print("That will be 30 dollars...")
+    else :
+        print("We dont serve that... Restart...")
+elif Order_Number_2 == "Delert" :
+    Dessert = input("Great! Would you like a chocolate crape or croissant")
+    if Dessert == "Chocolate Crape" :
+        print("That will be 20 dollars.")
+    elif Dessert == "Croissant" :
+        print("That will be 15 dollars...")
+else :
+    print("We are Timmy's Tacos... we obviously dont serve tacos or anything else here... lets try again...")
