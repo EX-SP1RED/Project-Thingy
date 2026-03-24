1af2e3fd5986c6708ea7ef2fb54e1ede5239692a
EX-SP1RED <ldcook0419@gmail.com>
2026-03-24 20:14:28 +0000

    Seventh Commit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ while(Do_it_again == "yes"):
         order.append(Order_Number_1)
         Cost = Cost + 85
     elif Order_Number_1 == "donut":
-        print("The donuts would have been 3 dollars, but because of our talented chefs... we will pay you 5 dollars...")
+        print("The donuts would have been 3 dollars, but because of our talented chefs... we will pay you 5 dollars... If you order this and waffles, you will get 3 more dollars off.")
         order.append(Order_Number_1)
         Cost = Cost - 5
     elif Order_Number_1 == "both" :
@@ -46,11 +46,26 @@ while(Do_it_again == "yes"):
             Cost = (Cost + 15)
     else :
         print("We are Timmy's Tacos... we obviously dont serve tacos or anything else here... lets try again...")
-    # TODO: Also request numeric input from user for order a certain quantity of a condiment.
-    
+
     Do_it_again = input("Would you like to order more/again?").strip().lower()
 
-# TODO: Make sure to apply discount to combo-eligible orders.
-# TODO: add more context to the printed output as to what these numbers or messages mean
+    # TODO: Also request numeric input from user for order a certain quantity of a condiment.
+Sauce_Option = input("Would you like to order a sauce?").strip().lower()
+if (Sauce_Option == "yes"):
+    Sauce_Choice = input("Would you like maple syrup or honey?").strip().lower()
+    if (Sauce_Choice == "maple syrup"):
+        Maple_Amount = int(input("How much would you like?"))
+        Cost = Cost + (1 * Maple_Amount)
+    else:
+        Honey_Amount = int(input("How much honey would you like?"))
+        Cost + (1.50 * Honey_Amount)
+else:
+    pass
+
+if (Order_Number_1 == "donut")and(Main_Course == "waffles"):
+    Cost -= 3
+else:
+    pass
+print("Your Order:")
 print(order)
 print(Cost)
